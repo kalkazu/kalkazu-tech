5a7cd414da2435dca44cb48020df08fb4d4d7050
kalkazu <bintiguinetworktech@gmail.com>
2025-05-27 23:32:21 -0300

    fix

--- src/app/websites/page.tsx
+++ src/app/websites/page.tsx
@@ -3,16 +3,21 @@
 
 
 
-import { SparklesBackground } from "@/components/sparklesBackground";
+
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 
-import React from "react";
+
 
 const page = () => {
   return (
     <div className="w-screen h-screen bg-black/[0.96] overflow-hidden">
-      <SparklesBackground />
-      
+      <Navbar />
+      <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+        Some of my Job <br /> I hope you enjoy it
+      </div>
+      <Footer />
     </div>
   );
 };
--- src/app/wordpress/page.tsx
+++ src/app/wordpress/page.tsx
@@ -1,21 +1,33 @@
-'use client';
-import Features from '@/components/Features';
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
-import Pricing from '@/components/Pricing';
-import { Spotlight } from '@/components/ui/spotlight';
-import React from 'react'
+"use client";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Pricing from "@/components/Pricing";
+import { Spotlight } from "@/components/ui/spotlight";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
     <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Navbar />
       <Spotlight className="-top-1/4 md:left-70 md:-top-1/4" fill="white" />
-        <Features />
-        <Pricing />
-        <Footer />
+      <div>
+        <Image 
+         src="/images/wordpresslogo.png" 
+         alt="Wordpress 3d logo" 
+         width={100} height={100}
+         className="mx-auto w-[300px] h-[300px]"
+         />
+        <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
+          Your Wordpress, <br /> Ecommerce or Website
+        </div>
+      </div>
+      <Features />
+      <Pricing />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
