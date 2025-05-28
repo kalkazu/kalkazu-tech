027098ba3c408711fac181dadaa78aa5ed7fe4ee
kalkazu <bintiguinetworktech@gmail.com>
2025-05-27 22:48:50 -0300

    fixed graphic

--- src/app/graphicdesign/page.tsx
+++ src/app/graphicdesign/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 import Footer from '@/components/Footer'
-import HeroSection from '@/components/HeroSection'
+
 import Navbar from '@/components/Navbar'
-
-
-
-
 import React from 'react'
 
 const page = () => {
   return (
     <div className='mx-auto'>
         <Navbar />
-        <HeroSection />
-
         <Footer />
     </div>
   )
